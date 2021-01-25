5a5888ecb95d3aec6ddbc85232e4199607e031e1
imsoumya18 <soumyadeep184@gmail.com>
2021-01-25 22:42:25 +0530

    first

--- main.py
+++ main.py
@@ -59,10 +59,9 @@ ws.delete_cols(47, 2)
 driver = webdriver.Chrome('chromedriver.exe')
 
 # Log in
-driver.get('https://ss.shipstation.com/')
-input('Hit "ENTER" after you have successfully logged in: ')
-driver.get('https://ship6.shipstation.com/orders/awaiting-shipment')
-input('loaded?')
+driver.get('https://ship6.shipstation.com/')
+time.sleep(1)
+input('Hit "ENTER" after the page is completely loaded: ')
 driver.find_elements_by_class_name('advanced-search-text-6ODW4Fd')[0].click()
 
 # Filling the columns
@@ -70,23 +69,23 @@ i = 2
 while i <= rows:
     driver.find_elements_by_class_name('flex-input-3IbU2o7')[0].clear()
     driver.find_elements_by_class_name('flex-input-3IbU2o7')[0].send_keys(str(ws.cell(i, 2).value))
-    driver.find_elements_by_id('ef1caaed-8e57-f0f4-17ae-ebf20af5bd20')[0].clear()
-    driver.find_elements_by_id('ef1caaed-8e57-f0f4-17ae-ebf20af5bd20')[0].send_keys(str(ws.cell(i, 3).value))
-    # date_old = str(ws.cell(i, 12).value)[0:10]
-    # date_new = date_old[5:7] + '/' + date_old[8:10] + '/' + date_old[0:4]
+    driver.find_elements_by_class_name('flex-input-3IbU2o7')[1].clear()
+    driver.find_elements_by_class_name('flex-input-3IbU2o7')[1].send_keys(str(ws.cell(i, 3).value))
     driver.find_elements_by_class_name('full-width-input-3F2-Knx')[0].clear()
-    # driver.find_elements_by_class_name('full-width-input-3F2-Knx')[0].send_keys(date_new)
     driver.find_elements_by_id('advanced-search-submit-button')[0].click()
     time.sleep(1)
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
     try:
         store = soup.select('.message-Wl5p7I-')[0].getText()
-        ws.cell(i, 1).value = str(store)                                                       # Column A
+        # Column A
+        ws.cell(i, 1).value = str(store)
         total = soup.select('.currency-column-value-1VfNyxR')[0].getText()
-        ws.cell(i, 4).value = float(str(total)[1:])                                            # Column D
-        ws.cell(i, 4).value = ws.cell(i, 4).value - ws.cell(i, 5).value                        # Column F
-        driver.find_elements_by_class_name('checkbox-CUegX-s')[0].click()
+        # Column D
+        ws.cell(i, 4).value = float(str(total)[1:])
+        # Column F
+        ws.cell(i, 6).value = '=IF(M' + str(i) + '="COVID-19 Surcharge","covid surcharge",D' + str(i) + '-E' + str(i) + ')'
+        driver.find_elements_by_class_name('grid-rows-1E9Z-Ar')[1].click()
         driver.find_element_by_xpath("//div[contains(text(), 'Edit Tags')]").click()
         driver.find_element_by_xpath("//span[contains(text(), 'AUDITED')]").click()
         time.sleep(1)
@@ -100,13 +99,19 @@ ws.cell(rows+1, 3).value = 'Totals'
 ws.cell(rows+1, 4).value = '=SUM(D2:D' + str(rows) + ')'
 ws.cell(rows+1, 5).value = '=SUM(E2:E' + str(rows) + ')'
 ws.cell(rows+1, 6).value = '=AVERAGE(F2:F' + str(rows) + ')'
-ws.cell(rows+2, 4).value = '-E' + str(rows+1)
+ws.cell(rows+2, 4).value = '=-E' + str(rows+1)
 ws.cell(rows+4, 3).value = 'Gross profit'
 ws.cell(rows+4, 4).value = '=SUM(D' + str(rows+1) + ':D' + str(rows+2) + ')'
 ws.cell(rows+6, 3).value = 'Gross profit per parcel'
 ws.cell(rows+6, 4).value = '=D' + str(rows+4) +'/77'
 ws.cell(rows+8, 3).value = 'MARGIN'
 ws.cell(rows+8, 4).value = '=D' + str(rows+4) + '/D' + str(rows+1)
+
+# Setting each column width to 30
+i = 'A'
+while i != 'X':
+    ws.column_dimensions[i].width = 30
+    i = chr(ord(i) + 1)
 
 # Save the file
 wb.save('GLOBEGISTICS ' + date + ' - INVOICE ' + inv + '.xlsx')
